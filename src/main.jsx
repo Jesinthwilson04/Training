ca1d1003850b2899d453953ead349e00e65972a3
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-03-13 23:01:13 +0530

    day3

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,22 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
-import './main.css';
-
+import {createRoot} from 'react-dom/client';
 const root=createRoot(document.getElementById("root"));
-function Header(){
-    return(
-        <header>
-            <img src="/react.svg" alt="react logo" width="40px"/>
-        </header>
-    );
-}
-function Body()
+function Header()
 {
     return(
-        <h1>
-            This is sample page for the learning purpose
-        </h1>
-    );
+        <h1>Welcome to page chief.</h1>
+    )
 }
-function Footer(){
+function Page()
+{
     return(
-        <footer>
-            ©️ 2024 Jesinthwilson development.All rights reserved.
-        </footer>
+        <>
+        <Header/>
+        <h2>this is subheader</h2>
+        <h3>hey folks how are you</h3>
+        </>
     );
 }
-
-
-
 root.render(
-    <div>
-    <Header/>
-    <Body/>
-    <Footer/>
-    </div>
-    
-);
+    <Page/>
+)

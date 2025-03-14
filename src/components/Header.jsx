f4de19923ce053bfd4d7c3ba5f3f73b16162abac
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-03-14 21:36:42 +0530

    third

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header()
+{
+    return(
+        <div className="header">
+        <img src="react.svg"/>
+        <h1>React Facts</h1>
+        </div>
+    );
+}
---
+++ src/components/Maincomponent.jsx
@@ -0,0 +1,15 @@
+ // Ensure correct path
+export default function Maincomponent()
+{
+    return(
+        <div className="main">
+        <h1 className="main2">Fun Facts about React</h1>
+        <ul>
+            <li>was released in 2013</li>
+            <li>is a JavaScript library</li>
+            <li>is used for building user interfaces</li>
+            <li>is maintained by Facebook</li>
+        </ul>
+        </div>
+    );
+}
